72895ccbee3732ce1d12e6e8334379ef84e18d79
Hit-Bheda <hitbheda.h@gmail.com>
2024-04-14 14:35:09 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,10 @@ const app = express()
 const router = require('./routes')
 require('dotenv').config()
 
+// app.use((req,res,next)=>{
+//     res.setHeader('Content-Type','application/json')
+//     next()
+// })
 app.use('/api', router)
 
 app.listen(process.env.PORT, () => {
